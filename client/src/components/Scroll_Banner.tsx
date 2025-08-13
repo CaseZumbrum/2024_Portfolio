96570e4e08ca3ff19b2fa7e20ed6a7ebf5b265ea
casezumbrum <casezumbrum@ufl.edu>
2025-08-13 01:03:57 -0400

    add skills

--- client/src/components/Scroll_Banner.tsx
+++ client/src/components/Scroll_Banner.tsx
@@ -1,22 +1,112 @@
 import "../styles/Scroll_Banner.css";
 
 const Scroll_Banner = () => {
-    return (
-
-
-        <div className="marquee marquee--8">
-            <img className="marquee__item" src="/static/images/github.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/linux.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/python.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/node.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/TS.png" width="100" height="100" style={{cursor:"pointer"}} onClick={()=>{(window.open("/project_pages/vector_fields.html", "_blank"))}} alt="" />
-            <img className="marquee__item" src="/static/images/mongo.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/fusion.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/matplot.png" width="100" height="100" alt="" />
-            <img className="marquee__item" src="/static/images/rpi.png" width="100" height="100" alt="" />
-        </div>
-
-    )
-}
+  return (
+    <div className="marquee marquee--8">
+      <img
+        className="marquee__item"
+        src="/static/images/github.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/linux.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/python.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/docker.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/node.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/TS.png"
+        width="100"
+        height="100"
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          window.open("/project_pages/vector_fields.html", "_blank");
+        }}
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/mongo.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/sql.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/matplot.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/pyqtgraph.jpg"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/rpi.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/quartus.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/vivado.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+      <img
+        className="marquee__item"
+        src="/static/images/fusion.png"
+        width="100"
+        height="100"
+        alt=""
+      />
+    </div>
+  );
+};
 
 export default Scroll_Banner;
